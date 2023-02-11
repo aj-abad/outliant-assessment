ce77c5d1d48b08d2c02c0341f08d5fa346bf3dd0
AJ <dev@ajabad.com>
2023-02-11 12:45:37 +0800

    Added more styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,12 +6,12 @@ export default <Partial<Config>>{
     },
     extend: {
       colors: {
-        "light": "#ffffff",
-        "secondary": "#ececec",
-        "text": "#000000",
-        'low-emphasis': '#636262',
-        'img-1': '#c4c4c4',
-        'img-2': '#9d9d9d',
+        light: "#ffffff",
+        secondary: "#ececec",
+        text: "#000000",
+        "low-emphasis": "#636262",
+        "img-1": "#c4c4c4",
+        "img-2": "#9d9d9d",
       },
     },
   },

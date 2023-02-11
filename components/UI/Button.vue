43de2d653c31cece92f54389adac8551dcbef1ad
AJ <dev@ajabad.com>
2023-02-11 20:01:43 +0800

    Added more styles

--- components/UI/Button.vue
+++ components/UI/Button.vue
@@ -1,7 +1,7 @@
 <template>
   <button
-    class="relative border-2 rounded-md px-6 py-3 font-medium select-none outline-none focus:ring-2 focus:ring-offset-2 focus:ring-opacity-50 overflow-hidden"
-    :class="variantButtonClass.get(variant) ?? null">
+    class="relative border-2 rounded-md px-6 py-3 font-medium select-none outline-none focus:ring-2 focus:ring-offset-2 focus:ring-opacity-50 overflow-hidden disabled:opacity-50 disabled:cursor-not-allowed"
+    :class="variantButtonClass.get(variant) ?? variantButtonClass.get('default')">
     <div
       class="overlay absolute h-full w-full top-0 left-0 pointer-events-none"
       aria-hidden
@@ -21,6 +21,7 @@ const { variant } = toRefs(props);
 const variantButtonClass = new Map<string, string>([
   ["primary", "bg-black border-text text-light focus:ring-text"],
   ["outlined", "bg-light text-text border-text"],
+  ["default", "bg-light text-text border-text"],
 ]);
 
 const variantOverlayClass = new Map<string, string>([

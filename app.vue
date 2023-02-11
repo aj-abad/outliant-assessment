5e16acb9bf99e677450c5a3477401e9c0f86b868
AJ <dev@ajabad.com>
2023-02-11 12:44:50 +0800

    Moved nav to its own component

--- app.vue
+++ app.vue
@@ -1,15 +1,6 @@
 <template>
-  <div class="bg-light">
-    <div class="container py-8 flex items-center">
-      <h1 class="font-semibold text-4xl shrink-0">Logo <span class="text-low-emphasis">.</span></h1>
-      <nav class="px-4 grow flex gap-16 justify-center items-center font-medium">
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Services</a>
-        <a href="#">Products</a>
-      </nav>
-      <UIButton variant="primary" type="button"> Call to action </UIButton>
-    </div>
+  <div class="bg-light text-text">
+    <AppNav />
     <NuxtPage />
   </div>
 </template>
---
+++ components/AppNav.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container py-4 md:py-8 flex items-center">
+    <h1 class="select-none font-semibold text-2xl md:text-4xl shrink-0">
+      Logo <span class="text-img-1 font-bold">.</span>
+    </h1>
+    <div class="hidden md:flex grow items-center">
+      <nav id="app-nav" class="px-4 grow flex gap-16 justify-center items-center font-medium">
+        <NuxtLink to="/">Home</NuxtLink>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <NuxtLink to="/products">Products</NuxtLink>
+      </nav>
+      <UIButton variant="primary" type="button"> Call to action </UIButton>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+#app-nav a {
+  position: relative;
+  &:not(.router-link-active) {
+    @apply text-text text-opacity-50;
+  }
+  &.router-link-active::before {
+    content: "";
+    background: currentColor;
+    position: absolute;
+    bottom: -4px;
+    left: 0;
+    right: 0;
+    height: 2px;
+  }
+}
+</style>

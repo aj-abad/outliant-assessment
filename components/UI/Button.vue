9bd87093b9ef2a604464c37cd3f63d664cc15466
AJ <dev@ajabad.com>
2023-02-11 21:25:52 +0800

    Changed styles

--- components/UI/Button.vue
+++ components/UI/Button.vue
@@ -3,7 +3,7 @@
     class="relative border-2 rounded-md px-6 py-3 font-medium select-none outline-none focus:ring-2 focus:ring-offset-2 focus:ring-opacity-50 overflow-hidden disabled:opacity-50 disabled:cursor-not-allowed"
     :class="variantButtonClass.get(variant) ?? variantButtonClass.get('default')">
     <div
-      class="overlay absolute h-full w-full top-0 left-0 pointer-events-none"
+      class="overlay absolute h-full w-full top-0 left-0 pointer-events-none bg-opacity-0"
       aria-hidden
       :class="variantOverlayClass.get(variant) ?? null"></div>
     <slot />
@@ -20,13 +20,13 @@ const { variant } = toRefs(props);
 
 const variantButtonClass = new Map<string, string>([
   ["primary", "bg-black border-text text-light focus:ring-text"],
-  ["outlined", "bg-light text-text border-text"],
+  ["outlined", "bg-light text-text border-text focus:ring-text"],
   ["default", "bg-light text-text border-text"],
 ]);
 
 const variantOverlayClass = new Map<string, string>([
-  ["primary", "bg-text bg-opacity-10"],
-  ["outlined", "bg-text bg-opacity-10"],
+  ["primary", "bg-white"],
+  ["outlined", "bg-text"],
 ]);
 </script>
 
@@ -35,7 +35,7 @@ button {
   &:hover,
   &:focus {
     .overlay {
-      @apply bg-white bg-opacity-10;
+      @apply bg-opacity-10;
     }
   }
 }
